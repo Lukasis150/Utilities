2ad7bca2fd4e10540aed0afdbd539e0dd0009533
Lukas Starenko <starenkol@gmail.com>
2019-07-29 09:37:38 +0200

    Add blank functions

--- DateUtils/src/DateUtils.php
+++ DateUtils/src/DateUtils.php
@@ -1,6 +1,23 @@
 <?php
 
+declare (strict_types = 1);
+
 class DateUtils 
 {
+
+}
+
+public static function getDateTime($date)
+{
     
+}
+
+public static function formatDate($date)
+{
+    
+}
+
+public static function formatDateTime($date)
+{
+   
 }
