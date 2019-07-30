72063f14fce1832ca5ff59cfc39332a073386ae8
Lukas Starenko <starenkol@gmail.com>
2019-07-30 13:26:49 +0200

    Add array messages

--- DateUtils/src/DateUtils.php
+++ DateUtils/src/DateUtils.php
@@ -11,10 +11,16 @@ class DateUtils
     const DATETIME_FORMAT = 'j.n.Y G:i:s';
     const DATE_FORMAT = 'j.n.Y';
     const TIME_FORMAT = 'G:i:s';
-    
+
     const DB_DATETIME_FORMAT = 'Y-m-d H:i:s';
     const DB_DATE_FORMAT = 'Y-m-d';
     const DB_TIME_FORMAT = 'H:i:s';
+    
+    private static $errorMessages = array(
+            self::DATE_FORMAT => 'Neplatné datum, zadejte ho prosím ve tvaru dd.mm.rrrr',
+            self::TIME_FORMAT => 'Neplatný čas, zadejte ho prosím ve tvaru hh:mm, můžete dodat i vteřiny',
+            self::DATETIME_FORMAT => 'Neplatné datum nebo čas, zadejte prosím hodnotu ve tvaru dd.mm.rrrr hh:mm, případně vteřiny',
+    );
 
     public static function getDateTime($date) :DateTime
     {
