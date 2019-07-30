4f3573878a1695c5629e4bc39b18afd24eb70e88
Lukas Starenko <starenkol@gmail.com>
2019-07-30 13:16:53 +0200

    Fill methods

--- DateUtils/src/DateUtils.php
+++ DateUtils/src/DateUtils.php
@@ -2,22 +2,29 @@
 
 declare (strict_types = 1);
 
+namespace Lukas\App;
+
+use DateTime;
+
 class DateUtils 
 {
+    public static function getDateTime($date) :DateTime
+    {
+        if (ctype_digit($date)) {
+            $date = '@' . $date;
+        }
+        return new DateTime($date);
+    }
 
-}
+    public static function formatDate($date) :string
+    {
+        $dateTime = self::getDateTime($date);
+        return $dateTime->format('d.m.Y');
+    }
 
-public static function getDateTime($date)
-{
-    
-}
-
-public static function formatDate($date)
-{
-    
-}
-
-public static function formatDateTime($date)
-{
-   
+    public static function formatDateTime($date) :string
+    {
+        $dateTime = self::getDateTime($date);
+        return $dateTime->format('d.m.Y H:i:s');
+    }
 }
