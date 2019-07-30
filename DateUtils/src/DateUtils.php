e3236901d472d9642d327a0bb7276b475938bd32
Lukas Starenko <starenkol@gmail.com>
2019-07-30 13:27:27 +0200

    Add array months in Czech

--- DateUtils/src/DateUtils.php
+++ DateUtils/src/DateUtils.php
@@ -15,12 +15,14 @@ class DateUtils
     const DB_DATETIME_FORMAT = 'Y-m-d H:i:s';
     const DB_DATE_FORMAT = 'Y-m-d';
     const DB_TIME_FORMAT = 'H:i:s';
-    
+
     private static $errorMessages = array(
             self::DATE_FORMAT => 'Neplatné datum, zadejte ho prosím ve tvaru dd.mm.rrrr',
             self::TIME_FORMAT => 'Neplatný čas, zadejte ho prosím ve tvaru hh:mm, můžete dodat i vteřiny',
             self::DATETIME_FORMAT => 'Neplatné datum nebo čas, zadejte prosím hodnotu ve tvaru dd.mm.rrrr hh:mm, případně vteřiny',
     );
+
+    private static $months = array('ledna', 'února', 'března', 'dubna', 'května', 'června', 'července', 'srpna', 'září', 'října', 'listopadu', 'prosince');
 
     public static function getDateTime($date) :DateTime
     {
