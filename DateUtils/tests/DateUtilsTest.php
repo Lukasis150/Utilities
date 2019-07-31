d3055a8b8262d2801754d91c24a1acd48e2776e8
Lukas Starenko <starenkol@gmail.com>
2019-07-31 12:44:33 +0200

    Add validate function to test

--- DateUtils/tests/DateUtilsTest.php
+++ DateUtils/tests/DateUtilsTest.php
@@ -22,4 +22,11 @@ class DateUtilsTest extends TestCase
         $dateUtils = new DateUtils();
         self::assertEquals('03.12.2018 13:06:34', $dateUtils->formatDateTime(date('2018-12-03 13:06:34.3870')));
     }
+    
+    public function testValidate()
+    {
+        $dateUtils = new DateUtils();
+        self::assertEquals(false, $dateUtils->validDate(date('2018-12-03 13:06:71.387125')));
+        // self::assertEquals(true, $dateUtils->validDate(date('2018-12-03 13:06:51.387125'), 'j.n.Y G:i:s'));
+    }
 }
